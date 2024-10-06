3e3d3dc61660ff432e4fecbf001eba763ca37137
Ruslan Hamidov <ruslanhamidov@Ruslans-MBP.localdomain>
2024-10-06 19:14:03 +0400

    change models and controllers, create artist model instances

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
 	has_secure_password
 
+	has_many :records
+
 	#validates :email, presence: true
 	#validates :password_digest, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_09_23_212925) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
